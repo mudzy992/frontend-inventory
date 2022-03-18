a337d83bdc3136a06f207d8af417fc6df895e75b
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2022-03-19 00:46:44 +0100

    Update HomePage.tsx

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -26,9 +26,9 @@ interface CategoryDto {
 
 /* Ova komponenta je proširena da se prikazuje na osnovu parametara koje smo definisali iznad */
 export default class HomePage extends React.Component {
-    state: HomePageState ;
+    state: HomePageState;
 
-    constructor(props: Readonly<{}>){
+    constructor(props: Readonly<{}>) {
         super(props);
         this.state = {
             categories: [],
@@ -45,7 +45,7 @@ export default class HomePage extends React.Component {
         this.setState(newState);
     }
 
-    private putCategoriesInState (data: CategoryDto[]){
+    private putCategoriesInState(data: CategoryDto[]) {
         const categories: CategoryType[] = data.map(category => {
             return {
                 categoryId: category.categoryId,
@@ -55,8 +55,8 @@ export default class HomePage extends React.Component {
         })
         const newState = Object.assign(this.state, {
             categories: categories
-          })
-          this.setState(newState)
+        })
+        this.setState(newState)
     }
 
     /* KRAJ SET FUNCKIJA */
@@ -69,24 +69,24 @@ export default class HomePage extends React.Component {
             );
         }
         /* kraj provjera */
-        return(
+        return (
             /* prikaz klijentu */
-            <Container style={{marginTop:15}}>
+            <Container style={{ marginTop: 15 }}>
                 <Card className="text-white bg-dark">
-                <Card.Body>
-                    <Card.Header>
-                        <Card.Title>
-                            <FontAwesomeIcon icon={faListAlt}/> Top level categories
-                        </Card.Title>
-                    </Card.Header>
-                    <Row>
-                        Treba definisati role za homepage
-                    </Row>
-                    <Row>
+                    <Card.Body>
+                        <Card.Header>
+                            <Card.Title>
+                                <FontAwesomeIcon icon={faListAlt} /> Top level categories
+                            </Card.Title>
+                        </Card.Header>
+                        <Row>
+                            Treba definisati role za homepage
+                        </Row>
+                        <Row>
                             {/* Ako je korisnik ulogovan, prikazati spisak kategorija 
                             to smo uradili tako što smo mapirali jednu funkciju ispod*/}
                             {this.state.categories.map(this.singleCategory)}
-                    </Row>
+                        </Row>
                     </Card.Body>
                 </Card>
             </Container>
@@ -95,33 +95,33 @@ export default class HomePage extends React.Component {
 
     private singleCategory(category: CategoryType) {
         return (
-        <Col lg="3" md="4" sm="6" xs="12">
-            <Card className="text-dark bg-light mb-3">
-                <Card.Header>
-                <Card.Title>
-                    {category.name}
-                </Card.Title>
-                </Card.Header>
-                <Card.Body>
-                    Ovjde će ići slika zvao se ja mudzy ili ne
-                {/* <Card.Img variant="top" src={ApiConfig.PHOTO_PATH + `${category.image}`} /> */}
-                </Card.Body>
-                <Card.Footer>
-                    <small><Link to={`/category/${category.categoryId}`}
-                    className='btn btn-primary btn-block btn-sm'>Prikaži kategoriju</Link></small> 
-                </Card.Footer>
-            </Card>
-        </Col>
+            <Col lg="3" md="4" sm="6" xs="12">
+                <Card className="text-dark bg-light mb-3">
+                    <Card.Header>
+                        <Card.Title>
+                            {category.name}
+                        </Card.Title>
+                    </Card.Header>
+                    <Card.Body>
+                        Ovjde će ići slika zvao se ja mudzy ili ne
+                        {/* <Card.Img variant="top" src={ApiConfig.PHOTO_PATH + `${category.image}`} /> */}
+                    </Card.Body>
+                    <Card.Footer>
+                        <small><Link to={`/category/${category.categoryId}`}
+                            className='btn btn-primary btn-block btn-sm'>Prikaži kategoriju</Link></small>
+                    </Card.Footer>
+                </Card>
+            </Col>
         )
     }
 
     /* GET I MOUNT FUNKCIJE ĆEMO DEFINISATI ISPOD RENDERA */
-    componentDidMount(){
+    componentDidMount() {
         /* Upisujemo funkcije koje se izvršavaju prilikom učitavanja stranice */
         this.getCategories()
     }
 
-    componentDidUpdate(){
+    componentDidUpdate() {
         /* Upisujemo logiku koja će se izvršavati nakon update (da se ne osvježava stalno stranica) */
     }
 
@@ -131,15 +131,15 @@ export default class HomePage extends React.Component {
     1. ruta (provjeriti u backend), 
     2. method (onaj koji definišemo u api da koristimo get, post, patch, delete, update..) 
     3. body (ako je get tj. prazan body stavljamo {} a ako nije unutar {definišemo body}) */
-    private getCategories () {
-        api('api/category/?filter=parentCategoryId||$isnull', 'get', {}, 'administrator' || 'user')
-        .then((res: ApiResponse) => {
-            if (res.status === 'login') {
-                return this.setLogginState(false);
-            }
-            /* Nakon što se izvrši ruta, šta onda */
-            this.putCategoriesInState(res.data)
-        }) 
+    private getCategories() {
+        api('api/category/?filter=parentCategoryId||$isnull', 'get', {}, 'administrator')
+            .then((res: ApiResponse) => {
+                if (res.status === 'login') {
+                    return this.setLogginState(false);
+                }
+                /* Nakon što se izvrši ruta, šta onda */
+                this.putCategoriesInState(res.data)
+            })
     }
 } /* Kraj koda */
     /* KRAJ GET I MOUNT FUNKCIJA */

7c49f656f2464c52b4fd58131b85704b07f139fc
Mudzahid Ceric <m.ceric@epbih.ba>
2022-11-20 18:59:45 +0100

    Update api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const ApiConfig = {
-    API_URL: 'https://invnetory-backend.web.app',
+    API_URL: 'https://mudzy-inventory-backend.herokuapp.com/',
     TIMEOUT: 15000,
     TEMPLATE_PATH: 'http://192.168.12.7:3005/prenosnica/'
     /* DODATI I PHOTO PATH AKO BUDE A BITi ĆE */

bb56d09a16abf143c5162eefd04adf749536406d
Mudzahid Ceric <m.ceric@epbih.ba>
2022-11-20 23:24:32 +0100

    Fire

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const ApiConfig = {
+    /* API_URL: 'http://192.168.0.13:3005/', */
     API_URL: 'https://mudzy-inventory-backend.herokuapp.com/',
     TIMEOUT: 15000,
     TEMPLATE_PATH: 'http://192.168.12.7:3005/prenosnica/'

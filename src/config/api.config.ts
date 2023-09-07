876ac596eb681df7d0f61dd7b7c6010b1b56baae
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2023-09-07 15:14:38 +0200

    Update api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,7 +1,8 @@
 export const ApiConfig = {
     /* API_URL: 'http://192.168.0.13:3005/', */
     /* API_URL: 'https://inventory-production-593a.up.railway.app/', */
-    API_URL: 'https://sore-lime-caridea-veil.cyclic.app/',
+    /*API_URL: 'https://sore-lime-caridea-veil.cyclic.app/',*/
+    API_URL: 'http://localhost:3005/',
     TIMEOUT: 15000,
     TEMPLATE_PATH: 'http://192.168.12.7:3005/prenosnica/'
     /* DODATI I PHOTO PATH AKO BUDE A BITi ĆE */

2077995fc8cb85b225197e12db0d6b4730912a5a
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2023-10-10 12:49:28 +0200

    Xd

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const ApiConfig = {
-    API_URL: 'http://inventory-back.ddns.net/',
+    API_URL: 'http://77.221.21.7',
     //API_URL: 'https://sore-lime-caridea-veil.cyclic.app',
     TIMEOUT: 15000,
     TEMPLATE_PATH: 'http://inventory-back.ddns.net/prenosnica/'

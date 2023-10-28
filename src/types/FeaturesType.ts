ffd4edffb1db21fd7a7ed2293f341ccb43d9844d
Mudzahid Ceric <m.ceric@epbih.ba>
2023-10-28 21:09:00 +0200

    Novi kod

--- src/types/FeaturesType.ts
+++ src/types/FeaturesType.ts
@@ -1,7 +1,10 @@
+import ArticleFeatureType from "./ArticleFeatureType";
+
 export default class FeaturesType {
     articleId?: number;
     name?: string;
     value?: string;
     featureId?: number;
     categoryId?: number;
+    articleFeatures?: ArticleFeatureType[];
 }

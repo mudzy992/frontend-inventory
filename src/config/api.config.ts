45f1d6e8ba2cbc87049027b380ecdf9273811339
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2023-10-31 23:51:44 +0100

    Update api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,9 +1,10 @@
+const url = 'http://77.221.31.121';
 export const ApiConfig = {
-    API_URL: 'http://77.221.31.121',
+    API_URL: url,
     //API_URL: 'http://inventory-back.ddns.net/',
     //API_URL: 'http://192.168.12.144:3005/', 
     //API_URL: 'https://sore-lime-caridea-veil.cyclic.app',
     TIMEOUT: 15000,
-    TEMPLATE_PATH: 'http://inventory-back.ddns.net/prenosnica/'
+    TEMPLATE_PATH: url + '/prenosnica/'
     /* DODATI I PHOTO PATH AKO BUDE A BITi ĆE */
 }

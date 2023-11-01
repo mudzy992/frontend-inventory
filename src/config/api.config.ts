f97206ad225b1d3f83425033dd8d17ad1e824712
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2023-11-01 20:27:43 +0100

    Update api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -6,5 +6,5 @@ export const ApiConfig = {
     //API_URL: 'https://sore-lime-caridea-veil.cyclic.app',
     TIMEOUT: 15000,
     TEMPLATE_PATH: url + '/prenosnica/'
-    /* DODATI I PHOTO PATH AKO BUDE A BITi ĆE */
+    /* DODATI I PHOTO PATH AKO BUDE A BITI ĆE */
 }

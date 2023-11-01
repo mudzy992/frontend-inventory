cc54a8674780e0abddd524c696b1b6cf2dcf7861
Mudzahid Ceric <m.ceric@epbih.ba>
2023-11-02 00:57:41 +0100

    Update ecosystem.config.js

--- deploy-scripts/ecosystem.config.js
+++ deploy-scripts/ecosystem.config.js
@@ -1,11 +1,10 @@
-module.exports = {
+/* module.exports = {
     apps : [{
       name: 'inventory-frontend', // Zamijenite s odgovarajućim imenom za vašu aplikaciju
-      script: 'serve',
-      args: 'build -p 4001',
-      interpreter: 'bash',
+      script: 'npm',
+      args: 'start',
+      interpreter: 'none',
       env: {
-        PATH: '/usr/local/bin/',
         NODE_ENV: 'production',
         PORT:4001, //Port obrisati ako ne rad
       },
@@ -17,14 +16,28 @@ module.exports = {
         host : '77.221.31.121',
         ref  : 'origin/AI-optimizacija', // Zamijenite s imenom vaše produkcione grane
         repo : 'https://github.com/mudzy992/frontend-inventory.git', // Zamijenite sa URL-om vašeg repozitorija
-        path : '/home/administrator/Documents/GitHub/frontend-inventory/', // Zamijenite sa stvarnom putanjom na vašem servera
+        path : '/home/administrator/Documents/GitHub/frontend-inventory/', // Zamijenite sa stvarnom putanjom na vašem server
         'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       }
     }
+  }; */
+  
+  
+  module.exports = {
+    apps: [
+      {
+        name: 'inventory-frontend',
+        script: 'serve',
+        args: 'build -p 4001',
+        interpreter: 'bash',
+        env: {
+          PATH: '/usr/local/bin/',
+          NODE_ENV: 'production',
+          PORT: 4001,
+        },
+      },
+    ],
   };
-  
-  
-  
   
   
   

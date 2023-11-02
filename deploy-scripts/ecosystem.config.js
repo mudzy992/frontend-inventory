4ce1a181c5a7790d3e2ec2504ed91b94aa5670dd
Mudzahid Ceric <m.ceric@epbih.ba>
2023-11-02 18:50:53 +0100

    Update ecosystem.config.js

--- deploy-scripts/ecosystem.config.js
+++ deploy-scripts/ecosystem.config.js
@@ -1,32 +1,27 @@
 module.exports = {
     apps : [{
-      name: 'inventory-frontend', // Zamijenite s odgovarajućim imenom za vašu aplikaciju
+      name: 'inventory-frontend',
       script: 'npm',
       args: 'start',
       interpreter: 'node',
       env: {
         NODE_ENV: 'production',
-        PORT:4001, 
+        PORT:4001,
       },
-      error_file: 'logs/error.log', // Putanja do datoteke za greške u podfolderu logs
-      out_file: 'logs/out.log',     // Putanja do datoteke za standardni izlaz u podfolderu logs
-      log_file: 'logs/all.log',     // Putanja do datoteke za sve zapise u podfolderu logs
+      error_file: 'logs/error.log', 
+      out_file: 'logs/out.log',     
+      log_file: 'logs/all.log', 
       time: true,  
     }],
   
     deploy : {
       production : {
-        user : 'administrator', // Zamijenite s vašim SSH korisničkim imenom
+        user : 'administrator',
         host : '77.221.31.121',
-        ref  : 'origin/AI-optimizacija', // Zamijenite s imenom vaše produkcione grane
-        repo : 'https://github.com/mudzy992/frontend-inventory.git', // Zamijenite sa URL-om vašeg repozitorija
-        path : '/home/administrator/Documents/GitHub/frontend-inventory/', // Zamijenite sa stvarnom putanjom na vašem server
+        ref  : 'origin/AI-optimizacija', 
+        repo : 'https://github.com/mudzy992/frontend-inventory.git',
+        path : '/home/administrator/Documents/GitHub/frontend-inventory/', /* Test */
         'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      }
+      } 
     }
   };
-  
-  
-  
-  
-  

e38b4d3b05d2c87a614ec56790b1cddbf074a7c8
Mudzahid Ceric <m.ceric@epbih.ba>
2023-11-02 18:28:02 +0100

    Logs

--- deploy-scripts/ecosystem.config.js
+++ deploy-scripts/ecosystem.config.js
@@ -1,13 +1,17 @@
-/* module.exports = {
+module.exports = {
     apps : [{
       name: 'inventory-frontend', // Zamijenite s odgovarajućim imenom za vašu aplikaciju
       script: 'npm',
       args: 'start',
-      interpreter: 'none',
+      interpreter: 'node',
       env: {
         NODE_ENV: 'production',
-        PORT:4001, //Port obrisati ako ne rad
+        PORT:4001, 
       },
+      error_file: 'logs/error.log', // Putanja do datoteke za greške u podfolderu logs
+      out_file: 'logs/out.log',     // Putanja do datoteke za standardni izlaz u podfolderu logs
+      log_file: 'logs/all.log',     // Putanja do datoteke za sve zapise u podfolderu logs
+      time: true,  
     }],
   
     deploy : {
@@ -18,30 +22,6 @@
         repo : 'https://github.com/mudzy992/frontend-inventory.git', // Zamijenite sa URL-om vašeg repozitorija
         path : '/home/administrator/Documents/GitHub/frontend-inventory/', // Zamijenite sa stvarnom putanjom na vašem server
         'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      }
-    }
-  }; */
-  
-  module.exports = {
-    apps: [{
-      name: 'inventory-frontend',
-      script: './deploy-scripts/build -p 4002',
-      /* args: [''], */ // Relativna putanja do build direktorija
-      interpreter: 'none',
-      env: {
-        PATH: '/usr/local/bin/serve',
-        PORT: '4002',
-        NODE_ENV: 'production',
-      },
-    }],
-    deploy: {
-      production: {
-        user: 'administrator',
-        host: '77.221.31.121',
-        ref: 'origin/AI-optimizacija',
-        repo: 'https://github.com/mudzy992/frontend-inventory.git',
-        path: '/home/administrator/Documents/GitHub/frontend-inventory/',
-        'post-deploy': 'npm install && pm2 reload deploy-scripts/ecosystem.config.js --env production', // Promenio se path za `ecosystem.config.js`
       }
     }
   };

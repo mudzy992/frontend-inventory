d278805def503f1804ad294b1a1666e7096978f5
Mudzahid Ceric <m.ceric@epbih.ba>
2023-11-10 17:40:31 +0100

    Update api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,9 +1,7 @@
-const url = 'http://77.221.31.121';
+/* const url = 'http://77.221.31.121'; //PRODUKCIJA */
+const url = 'http://inventory.hopto.org:3005'; // TEST
 export const ApiConfig = {
     API_URL: url,
-    //API_URL: 'http://inventory-back.ddns.net/',
-    //API_URL: 'http://192.168.12.144:3005/', 
-    //API_URL: 'https://sore-lime-caridea-veil.cyclic.app',
     TIMEOUT: 15000,
     TEMPLATE_PATH: url + '/prenosnica/'
     /* DODATI I PHOTO PATH AKO BUDE A BITI ĆE */

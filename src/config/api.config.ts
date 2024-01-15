b330ad262af0f10797f1841db79de2444c8e0ac0
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2024-01-15 09:03:25 +0100

    Delete

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
-const url = 'http://inventory.hopto.org:3005'; // PRODUKCIJA
-//const url = 'http://192.168.12.144:3005'; // LOKALNA POSAO
+//const url = 'http://inventory.hopto.org:3005'; // PRODUKCIJA
+const url = 'http://192.168.12.144:3005'; // LOKALNA POSAO
 //const url = 'http://192.168.0.13:3005'; // LOKALNA KUĆI
 export const ApiConfig = {
     API_URL: url,

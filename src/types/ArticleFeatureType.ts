044875e2fb0f61659b7c983843a36a381e9e1088
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2024-06-28 19:50:48 +0200

    Integracija sa postojećim printerima

--- src/types/ArticleFeatureType.ts
+++ src/types/ArticleFeatureType.ts
@@ -2,6 +2,7 @@ import ArticleType from "./ArticleType";
 import FeaturesType from "./FeaturesType";
 
 export default class ArticleFeatureType {
+    articleFeatureId?: number;
     articleId?: number;
     featureId?: number;
     featureValue?: string;

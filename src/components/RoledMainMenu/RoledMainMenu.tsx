071b60f1bb962e6aa0dd874a3193e3306c9e5f27
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2024-07-22 22:15:21 +0200

    Update RoledMainMenu.tsx

--- src/components/RoledMainMenu/RoledMainMenu.tsx
+++ src/components/RoledMainMenu/RoledMainMenu.tsx
@@ -13,7 +13,7 @@ const RoledMainMenu: React.FC = () => {
   ];
 
   const getAdministratorItems = (): MainMenuItem[] => [
-    { text: "Naslovna", link: "#/" },
+    { text: "Naslovna1", link: "#/" },
     { text: "Helpdesk", link: "#/admin/helpdesk" },
     { text: "SNMP", link: "#/admin/snmp" },
   ];

6ba11b03c1fff5092d7b2e61bc32c15a55fcb56b
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2025-01-28 18:58:05 +0100

    Docker integer

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,7 +1,7 @@
-const url = 'http://inventory.hopto.org:3005'; // PRODUKCIJA
+const url = 'http://inventory-backend:3005'; // PRODUKCIJA
 //const url = "http://192.168.12.69:3006"; // LOKALNA POSAO
 //const url = 'http://192.168.0.29:3005'; // LOKALNA KUĆI
-//const url = 'http://192.168.0.33:3005'; // HOTSPOT
+//const url = 'http://192.168.0.33:3006'; // HOTSPOT
 export const ApiConfig = {
   API_URL: url,
   TIMEOUT: 15000,
